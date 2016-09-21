28b6ffeae05030f31d68f54c38e677a3c9e221a7
Dean Birznieks <deanb100gmail.com@Deans-MacBook-Pro-2.local>
2016-09-22 00:42:33 +1000

    Updated for Unit 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,13 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+  alert("You found a match!");
+} else if (cardTwo === cardThree) {
+  alert("You found a match!");
+} else if (cardTwo === cardOne) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again");
+}
